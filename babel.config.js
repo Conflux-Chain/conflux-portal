343f482b609629ac16b9543fe150b481bd02edfd
ricky <ricky.miller@gmail.com>
2020-02-18 10:11:52 -0500

    Add optional chaining (#8052)
    * Add plugin-proposal-optional-chaining
    
    * Use optional chaining to provide error fallback

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = function (api) {
       '@babel/plugin-transform-runtime',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-optional-chaining',
     ],
   }
 }
