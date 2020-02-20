03830426c48477080c11307ad6d45cdc581057e5
yqrashawn <namy.19@gmail.com>
2020-02-20 17:57:33 +0800

    Fix: handle getCode error

--- ui/app/pages/send/send.utils.js
+++ ui/app/pages/send/send.utils.js
@@ -213,8 +213,9 @@ async function estimateGas ({
 
   // if recipient has no code, gas is 21k max:
   if (!selectedToken && !data) {
-    const code = Boolean(to) && (await global.eth.getCode(to))
+    const code = Boolean(to) && (await global.eth.getCode(to).catch(() => {}))
     // Geth will return '0x', and ganache-core v2.2.1 will return '0x0'
+    // conflux will return error if there's no contract at that address
     const codeIsEmpty = !code || code === '0x' || code === '0x0'
     if (codeIsEmpty) {
       return SIMPLE_GAS_COST
